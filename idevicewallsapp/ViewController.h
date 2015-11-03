ccba267fd7d0015d3fa28d5f8437139a86220d38
JunesiPhone <street.visions@yahoo.com>
2015-11-02 21:10:04 -0600

    Initial Commit

---
+++ idevicewallsapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  idevicewallsapp
+//
+//  Created by Edward Winget on 11/2/15.
+//  Copyright © 2015 junesiphone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ idevicewallsapp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  idevicewallsapp
+//
+//  Created by Edward Winget on 11/2/15.
+//  Copyright © 2015 junesiphone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
